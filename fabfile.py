e653534a6aeaa3432cef6304d17c741c80cd6a6a
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2011-11-27 11:18:58 +0530

    Initial commit.

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+from fabric.api import task, env, local, run, cd, lcd, put
+
+env.hosts = ['sharat87@sharat87.webfactional.com']
+
+@task(default=True)
+def up():
+    tempdir = '/tmp/mit-license'
+    archive = '/tmp/mit-license.tar.bz2'
+
+    local('cp -r . ' + tempdir)
+
+    with lcd(tempdir):
+        local('rm -v fabfile.py*')
+        local('tar -cjf ' + archive + ' *')
+
+    put(archive)
+
+    with cd('~/webapps/mit_license'):
+        run('rm -rf ./*')
+        run('tar -xjf ~/' + archive)
+        run('rm ~/' + archive)
+
+    local('rm -rf ' + tempdir + ' ' + archive)
